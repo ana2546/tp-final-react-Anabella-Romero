8fb290d909df32a2ec940e796e4f7adedf631cbf
Ana Romero <aromero582@alumnos.iua.edu.ar>
2025-10-30 17:45:04 -0300

    agrega buscador en lista de pokemones y oculta paginacion en la busqueda

--- TP-frontend/src/pages/Home.tsx
+++ TP-frontend/src/pages/Home.tsx
@@ -1,7 +1,10 @@
 import { useFavorites } from "../hooks/useFavorites";
+import { useNavigate } from "react-router-dom";
 
 const Home: React.FC = () => {
   const { favorites } = useFavorites();
+  const navigate = useNavigate();
+  const { toggleFavorite, isFavorite } = useFavorites();
 
   return (
     <div className="container text-center">
@@ -13,7 +16,11 @@ const Home: React.FC = () => {
         <div className="row">
           {favorites.map((pokemon) => (
             <div key={pokemon.name} className="col-md-3 mb-4">
-              <div className="card text-center shadow-sm">
+              <div
+                className="card text-center shadow-sm"
+                style={{ cursor: "pointer" }}
+                onClick={() => navigate(`/pokemon/${pokemon.name}`)}
+              >
                 <img
                   src={pokemon.image}
                   alt={pokemon.name}
@@ -22,6 +29,24 @@ const Home: React.FC = () => {
                 />
                 <div className="card-body">
                   <h5 className="card-title text-capitalize">{pokemon.name}</h5>
+                  <button
+                    className="btn btn-outline-danger rounded-circle"
+                    style={{
+                      width: "40px",
+                      height: "40px",
+                      padding: 0,
+                      fontSize: "1.2rem",
+                    }}
+                    onClick={(e) => {
+                      e.stopPropagation(); // ⚡ Detiene el click de la card
+                      toggleFavorite({
+                        name: pokemon.name,
+                        image: pokemon.image,
+                      });
+                    }}
+                  >
+                    {isFavorite(pokemon.name) ? "❤️" : "🤍"}
+                  </button>
                 </div>
               </div>
             </div>
